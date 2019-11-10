76d9c21477b3953dc57ec99015b18855bd286ad7
80036nd <40246027+80036nd@users.noreply.github.com>
2019-11-10 16:17:07 -0500

    Update jailbreak.sh

--- jailbreak.sh
+++ jailbreak.sh
@@ -3,5 +3,5 @@ cd ~/Documents/CheckRa1n
 rm -rf checkra1n.app
 rm -rf __MACOSX
 unzip checkra1n.zip
-./checkra1n.app/Contents/MacOS/checkra1n_gui
+./checkra1n.app/Contents/MacOS/checkra1n_gui -
 killall Terminal
